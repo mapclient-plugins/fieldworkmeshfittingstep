6d12506cf36f47506cda448e7989af8b757053b0
TimmyNic <tim.nicolas@outlook.com>
2021-06-08 10:02:38 +1200

    Updated ConfigureDialog() arguments.

--- mapclientplugins/fieldworkmeshfittingstep/step.py
+++ mapclientplugins/fieldworkmeshfittingstep/step.py
@@ -247,7 +247,7 @@ class FieldworkMeshFittingStep(WorkflowStepMountPoint):
         then set:
             self._configured = True
         '''
-        dlg = ConfigureDialog()
+        dlg = ConfigureDialog(self._main_window)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
         dlg.validate()
